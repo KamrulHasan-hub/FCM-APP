21e6ca18ab4c04ac37fd604c5389d0a34210cacb
kamrul <kamrulhasankganj@gmail.com>
2024-10-02 11:51:51 +0600

    ami kamrul

---
+++ app/src/main/java/com/example/fcmapp/repository/FCMRepository.kt
@@ -0,0 +1,8 @@
+package com.example.fcmapp.repository
+
+class FCMRepository {
+    private val serverKey =""
+    private val fcmurl ="https:/fcm.google.com/fcm/send"
+
+    private val client =OkHttp
+}
